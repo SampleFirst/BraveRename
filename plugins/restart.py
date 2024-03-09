f797b00342d61f6ca2cce040ba0a84df0d8f8852
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-09 23:31:00 +0530

    Create restart.py

---
+++ plugins/restart.py
@@ -0,0 +1,32 @@
+import datetime
+import random
+import time
+import os
+import requests
+
+restart_time_minutes = random.randint(2, 5)
+restart_time_seconds = random.randint(0, 59)
+restart_time = datetime.timedelta(minutes=restart_time_minutes, seconds=restart_time_seconds)
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT")
+
+is_restarted = True
+
+def send_restart_message():
+    restart_message = f"⚡ Bot Restarted ⚡\n🥂 Time Taken: {restart_time_minutes} Minutes {restart_time_seconds} Seconds"
+    
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    data = {
+        "chat_id": SUPPORT_CHAT,
+        "text": restart_message
+    }
+    response = requests.post(url, json=data)
+    if response.status_code != 200:
+        print(f"Failed to send restart message. Error: {response.text}")
+
+time.sleep(10)
+
+if is_restarted:
+    send_restart_message()
+    
