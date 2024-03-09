db83ac1014f51f82345d83c852c593d349050704
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-09 22:44:29 +0530

    Create about.py

---
+++ plugins/about.py
@@ -0,0 +1,24 @@
+import os 
+from pyrogram import Client, filters
+from helper.database import botdata, find_one
+from helper.progress import humanbytes
+
+TOKEN = os.environ.get('TOKENS','')
+botid = TOKEN.split(':')[0]
+
+
+@app.on_message(filters.private & filters.command("about"))
+async def start(client,message):
+    botdata(int(botid))
+    data = find_one(int(botid))
+    total_rename = data["total_rename"]
+    total_size = data["total_size"]
+    await message.reply_text(
+        f"ʙᴏᴛ ɴᴀᴍᴇ: Brave\n"
+        f"ʟᴀɴɢᴜᴀɢᴇ: ᴘʏᴛʜᴏɴ3\n"
+        f"ʟɪʙʀᴀʀʏ: ᴘʏʀᴏɢʀᴀᴍ 2.0\n"
+        f"ꜱᴇʀᴠᴇʀ: ᴋᴏʏᴇʙ\n"
+        f"ᴛᴏᴛᴀʟ ʀᴇɴᴀᴍᴇᴅ ꜰɪʟᴇ: {total_rename}\n"
+        f"ᴛᴏᴛᴀʟ ꜱɪᴢᴇ ʀᴇɴᴀᴍᴇᴅ: {humanbytes(int(total_size))}\n\n"
+    )
+
