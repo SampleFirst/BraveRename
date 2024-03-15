24d06469b44ed313f33b1dd13bb89db494dac069
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-16 00:31:33 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,4 +1,4 @@
-import os 
+import os
 from pyrogram import Client, filters
 from helper.database import botdata, find_one
 from helper.progress import humanbytes
@@ -6,19 +6,18 @@ from info import BOT_TOKEN
 
 botid = BOT_TOKEN.split(':')[0]
 
-
 @Client.on_message(filters.private & filters.command("about"))
-async def start(client,message):
+async def about(client, message):
     botdata(int(botid))
     data = find_one(int(botid))
     total_rename = data["total_rename"]
     total_size = data["total_size"]
     await message.reply_text(
-        f"ʙᴏᴛ ɴᴀᴍᴇ: Brave\n"
-        f"ʟᴀɴɢᴜᴀɢᴇ: ᴘʏᴛʜᴏɴ3\n"
-        f"ʟɪʙʀᴀʀʏ: ᴘʏʀᴏɢʀᴀᴍ 2.0\n"
-        f"ꜱᴇʀᴠᴇʀ: ᴋᴏʏᴇʙ\n"
-        f"ᴛᴏᴛᴀʟ ʀᴇɴᴀᴍᴇᴅ ꜰɪʟᴇ: {total_rename}\n"
-        f"ᴛᴏᴛᴀʟ ꜱɪᴢᴇ ʀᴇɴᴀᴍᴇᴅ: {humanbytes(int(total_size))}\n\n"
+        f"Name: 4GB Rename\n"
+        f"Language: Python 3\n"
+        f"Library: Pyrogram 2.0\n"
+        f"Server: Koyeb\n"
+        f"Total Renamed Files: {total_rename}\n"
+        f"Total Size Renamed: {humanbytes(int(total_size))}\n\n"
     )
-
+    
