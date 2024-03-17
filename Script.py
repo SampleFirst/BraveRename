0780f5f2be06c7fa77cc1db50be1b59f9a29ffdf
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-17 15:10:32 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,8 +1,8 @@
 import random
 
 class script(object):
-    START_TEXT = """Hey! {},\nWelcome to the rename bot"""
-    SOURCE_TEXT = """<b>Note:</b>\n- Source Project is Not An open Source"""
+    START_TEXT = """ʜᴇʏ! {},\nᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ʀᴇɴᴀᴍᴇ ʙᴏᴛ"""
+    SOURCE_TEXT = """<b>Note:</b>\n- ꜱᴏᴜʀᴄᴇ ᴘʀᴏᴊᴇᴄᴛ ɪꜱ ɴᴏᴛ ᴀɴ ᴏᴘᴇɴ ꜱᴏᴜʀᴄᴇ"""
     RENDER_TEXT = f"""<b>ɴᴏᴛᴇ:</b>
  🌐 SYSTEM STATUS 🌐
 
@@ -13,18 +13,18 @@ class script(object):
 {"●" * random.randint(0, 2) + "○" * (2 - random.randint(0, 2))}
 
 v[2.3.4] stable"""
-    HELP_TEXT = """𝙷𝙴𝚈\nUpdated Help Text."""
-    ABOUT_TEXT = """𝙷𝙴𝚈\nUpdated About Text."""
+    HELP_TEXT = """ʜᴇʏ\nᴜᴘᴅᴀᴛᴇᴅ ʜᴇʟᴘ ᴛᴇxᴛ."""
+    ABOUT_TEXT = """ʜᴇʏ\nᴜᴘᴅᴀᴛᴇᴅ ᴀʙᴏᴜᴛ ᴛᴇxᴛ."""
     THUMBNAIL_TEXT = """<b>Note:</b>\n🌌How to Set Thumbnail\n\nSend any photo to me, and I will save it automatically.\n/delthumb : Use this command to delete old thumbnail. \n/viewthumb : Use this command to view your current thumbnail."""
     CAPTION_TEXT = """<b>Note:</b>\n📝 How to Set Caption\n\n/set_caption Set a custom caption for your content.\n/see_caption: To view your custom caption.\n/delete_caption: Remove your custom caption from my DB."""
     WONX_TEXT = """ʏᴏᴜʀ ꜰʀɪᴇɴᴅ ɪꜱ ᴀʟʀᴇᴀᴅʏ ᴜꜱɪɴɢ ᴏᴜʀ ʙᴏᴛ"""
-    WON_TEXT = """🎉 Congratulations! You Won 10GB Upload limit"""
-    SUB_TEXT = """🔒 Join update Channel for using bot"""
+    WON_TEXT = """🎉 ᴄᴏɴɢʀᴀᴛᴜʟᴀᴛɪᴏɴꜱ! ʏᴏᴜ ᴡᴏɴ 10ɢʙ ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ"""
+    SUB_TEXT = """🔒 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ꜰᴏʀ ᴜꜱɪɴɢ ʙᴏᴛ"""
     LOG_TEXT = """#New_User\nUSER_ID = {user_id}\nNAME = {message.from_user.mention}\nPLAN = {user}"""
-    FLOOD_TEXT = """Flood control is active so please wait for {ltime}"""
-    EXP_QUOTA_TEXT = """100% of daily {humanbytes(limit)} data quota exhausted.\nFile size detected {humanbytes(file.file_size)}\nUsed daily limit {humanbytes(used)}\nYou have only **{humanbytes(remain)}** left on your account.\nIf you want to rename large file upgrade your plan."""
-    DLIMIT_TEXT = """You can't upload more than {humanbytes(limit)} used daily limit {humanbytes(used)}"""
-    RENAME_TEXT = """What do you want me to do with this file?\n\n**File Name**: `{filename}`\n**File Size**: {humanize.naturalsize(file.file_size)}\n**DC ID**: {dcid}"""
-    EXP_TEXT = """Your plan expired on {buy_date}"""
-    GB_TEXT = """Can't upload files bigger than 2GB"""
+    FLOOD_TEXT = """ꜰʟᴏᴏᴅ ᴄᴏɴᴛʀᴏʟ ɪꜱ ᴀᴄᴛɪᴠᴇ ꜱᴏ ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ ꜰᴏʀ {ltime}"""
+    EXP_QUOTA_TEXT = """100% ᴏꜰ ᴅᴀɪʟʏ {humanbytes(limit)} ᴅᴀᴛᴀ Qᴜᴏᴛᴀ ᴇxʜᴀᴜꜱᴛᴇᴅ.\nꜰɪʟᴇ ꜱɪᴢᴇ ᴅᴇᴛᴇᴄᴛᴇᴅ {humanbytes(file.file_size)}\nᴜꜱᴇᴅ ᴅᴀɪʟʏ ʟɪᴍɪᴛ {humanbytes(used)}\nʏᴏᴜ ʜᴀᴠᴇ ᴏɴʟʏ **{humanbytes(remain)}** ʟᴇꜰᴛ ᴏɴ ʏᴏᴜʀ ᴀᴄᴄᴏᴜɴᴛ.\nɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ʀᴇɴᴀᴍᴇ ʟᴀʀɢᴇ ꜰɪʟᴇ ᴜᴘɢʀᴀᴅᴇ ʏᴏᴜʀ ᴘʟᴀɴ."""
+    DLIMIT_TEXT = """ʏᴏᴜ ᴄᴀɴ'ᴛ ᴜᴘʟᴏᴀᴅ ᴍᴏʀᴇ ᴛʜᴀɴ {humanbytes(limit)} ᴜꜱᴇᴅ ᴅᴀɪʟʏ ʟɪᴍɪᴛ {humanbytes(used)}"""
+    RENAME_TEXT = """ᴡʜᴀᴛ ᴅᴏ ʏᴏᴜ ᴡᴀɴᴛ ᴍᴇ ᴛᴏ ᴅᴏ ᴡɪᴛʜ ᴛʜɪꜱ ꜰɪʟᴇ?\n\n**File Name**: `{filename}`\n**File Size**: {humanize.naturalsize(file.file_size)}\n**DC ID**: {dcid}"""
+    EXP_TEXT = """ʏᴏᴜʀ ᴘʟᴀɴ ᴇxᴘɪʀᴇᴅ ᴏɴ {buy_date}"""
+    GB_TEXT = """ᴄᴀɴ'ᴛ ᴜᴘʟᴏᴀᴅ ꜰɪʟᴇꜱ ʙɪɢɢᴇʀ ᴛʜᴀɴ 2ɢʙ"""
     
