146eeca7ebe535d7f8b775a9c12a158f2911db84
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-18 13:02:14 +0530

    Update filedetect.py

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -3,97 +3,6 @@ from pyrogram.enums import MessageMediaType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
 from Script import script 
 
-async def rename_file():
-    try:
-        media = await bot.get_messages(message.chat.id, message.reply_to_message.id)
-        file = media.reply_to_message.document or media.reply_to_message.video or media.reply_to_message.audio
-        filename = file.file_name
-        types = file.mime_type.split("/")
-        mime = types[0]
-        mg_id = media.reply_to_message.id
-        try:
-            out = new_name.split(".")
-            out[1]
-            out_name = out[-1]
-            out_filename = new_name
-            await media.reply_to_message.delete()
-            if mime == "video":
-                markup = InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("📁 ᴅᴏᴄᴜᴍᴇɴᴛ", callback_data="doc"),
-                            InlineKeyboardButton("🎥 ᴠɪᴅᴇᴏ", callback_data="vid")
-                        ]   
-                    ]
-                )
-            elif mime == "audio":
-                markup = InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("📁 ᴅᴏᴄᴜᴍᴇɴᴛ", callback_data="doc"), 
-                            InlineKeyboardButton("🎵 ᴀᴜᴅɪᴏ", callback_data="aud")
-                        ]
-                    ]
-                )
-            else:
-                markup = InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("📁 ᴅᴏᴄᴜᴍᴇɴᴛ", callback_data="doc")
-                        ]
-                    ]
-                )
-            await message.reply_text(
-                text=script.SELECT_FILETYPE_TEXT.format(out_filename),
-                reply_to_message_id=mg_id, 
-                reply_markup=markup
-            )
-
-        except:
-            try:
-                out = filename.split(".")
-                out_name = out[-1]
-                out_filename = new_name + "." + out_name
-            except:
-                await media.reply_to_message.delete()
-                await media.reply_text("**ᴇʀʀᴏʀ** : ɴᴏ ᴇxᴛᴇɴꜱɪᴏɴ ɪɴ ꜰɪʟᴇ, ɴᴏᴛ ꜱᴜᴘᴘᴏʀᴛɪɴɢ", reply_to_message_id=mg_id)
-                return
-            await media.reply_to_message.delete()
-            if mime == "video":
-                markup = InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("📁 ᴅᴏᴄᴜᴍᴇɴᴛ", callback_data="doc"), 
-                            InlineKeyboardButton("🎥 ᴠɪᴅᴇᴏ", callback_data="vid")
-                        ]
-                    ]
-                )
-            elif mime == "audio":
-                markup = InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("📁 ᴅᴏᴄᴜᴍᴇɴᴛ", callback_data="doc"), 
-                            InlineKeyboardButton("🎵 ᴀᴜᴅɪᴏ", callback_data="aud")
-                        ]
-                    ]
-                )
-            else:
-                markup = InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("📁 ᴅᴏᴄᴜᴍᴇɴᴛ", callback_data="doc")
-                        ]
-                    ]
-                )
-            await message.reply_text(
-                text=script.SELECT_FILETYPE_TEXT.format(out_filename),
-                reply_to_message_id=mg_id, 
-                reply_markup=markup
-            )
-    except Exception as e:
-        print(f"error: {e}")
-
-
 @Client.on_message(filters.private & filters.reply)
 async def refunc(client, message):
     try:
@@ -188,3 +97,95 @@ async def refunc(client, message):
                 )
     except Exception as e:
         print(f"error: {e}")
+        
+        
+async def rename_file(client, message):
+    try:
+        media = await bot.get_messages(message.chat.id, message.reply_to_message.id)
+        file = media.reply_to_message.document or media.reply_to_message.video or media.reply_to_message.audio
+        filename = file.file_name
+        types = file.mime_type.split("/")
+        mime = types[0]
+        mg_id = media.reply_to_message.id
+        try:
+            out = new_name.split(".")
+            out[1]
+            out_name = out[-1]
+            out_filename = new_name
+            await media.reply_to_message.delete()
+            if mime == "video":
+                markup = InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("📁 ᴅᴏᴄᴜᴍᴇɴᴛ", callback_data="doc"),
+                            InlineKeyboardButton("🎥 ᴠɪᴅᴇᴏ", callback_data="vid")
+                        ]   
+                    ]
+                )
+            elif mime == "audio":
+                markup = InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("📁 ᴅᴏᴄᴜᴍᴇɴᴛ", callback_data="doc"), 
+                            InlineKeyboardButton("🎵 ᴀᴜᴅɪᴏ", callback_data="aud")
+                        ]
+                    ]
+                )
+            else:
+                markup = InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("📁 ᴅᴏᴄᴜᴍᴇɴᴛ", callback_data="doc")
+                        ]
+                    ]
+                )
+            await message.reply_text(
+                text=script.SELECT_FILETYPE_TEXT.format(out_filename),
+                reply_to_message_id=mg_id, 
+                reply_markup=markup
+            )
+
+        except:
+            try:
+                out = filename.split(".")
+                out_name = out[-1]
+                out_filename = new_name + "." + out_name
+            except:
+                await media.reply_to_message.delete()
+                await media.reply_text("**ᴇʀʀᴏʀ** : ɴᴏ ᴇxᴛᴇɴꜱɪᴏɴ ɪɴ ꜰɪʟᴇ, ɴᴏᴛ ꜱᴜᴘᴘᴏʀᴛɪɴɢ", reply_to_message_id=mg_id)
+                return
+            await media.reply_to_message.delete()
+            if mime == "video":
+                markup = InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("📁 ᴅᴏᴄᴜᴍᴇɴᴛ", callback_data="doc"), 
+                            InlineKeyboardButton("🎥 ᴠɪᴅᴇᴏ", callback_data="vid")
+                        ]
+                    ]
+                )
+            elif mime == "audio":
+                markup = InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("📁 ᴅᴏᴄᴜᴍᴇɴᴛ", callback_data="doc"), 
+                            InlineKeyboardButton("🎵 ᴀᴜᴅɪᴏ", callback_data="aud")
+                        ]
+                    ]
+                )
+            else:
+                markup = InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("📁 ᴅᴏᴄᴜᴍᴇɴᴛ", callback_data="doc")
+                        ]
+                    ]
+                )
+            await message.reply_text(
+                text=script.SELECT_FILETYPE_TEXT.format(out_filename),
+                reply_to_message_id=mg_id, 
+                reply_markup=markup
+            )
+    except Exception as e:
+        print(f"error: {e}")
+        
