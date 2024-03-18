c8a4a58f37bf1c550215f32795b4c9dc96d4f8d9
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-18 23:20:45 +0530

    Update thumbnail.py

--- plugins/thumbnail.py
+++ plugins/thumbnail.py
@@ -1,23 +1,38 @@
 from pyrogram import Client, filters
 from helper.database import find, delthumb, addthumb
 
-@Client.on_message(filters.private & filters.command(['viewthumb']))
-async def view_thumb(client, message):
-    print(message.chat.id)
-    thumb = find(int(message.chat.id))[0]
-    if thumb:
-        await client.send_photo(message.chat.id, photo=thumb)
-    else:
-        await message.reply_text("**You don't have any thumbnail**")
+# Global variable to track if user has set thumbnail or not
+user_thumbnail_set = {}
 
-@Client.on_message(filters.private & filters.command(['delthumb']))
-async def remove_thumb(client, message):
-    delthumb(int(message.chat.id))
-    await message.reply_text("**Thumbnail deleted successfully**")
+@Client.on_message(filters.private & filters.command(['set_thumb']))
+async def set_thumbnail_command(client, message):
+    user_id = message.from_user.id
+    user_thumbnail_set[user_id] = True
+    await message.reply_text("ᴘʟᴇᴀꜱᴇ ꜱᴇɴᴅ ᴛʜᴇ ᴘʜᴏᴛᴏ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ꜱᴇᴛ ᴀꜱ ᴛʜᴜᴍʙɴᴀɪʟ.")
 
 @Client.on_message(filters.private & filters.photo)
 async def add_thumbs(client, message):
+    user_id = message.from_user.id
+    if user_id not in user_thumbnail_set or not user_thumbnail_set[user_id]:
+        return
+    
     file_id = str(message.photo.file_id)
-    addthumb(message.chat.id, file_id)
-    await message.reply_text("**Thumbnail saved successfully** ✅")
+    addthumb(user_id, file_id)
+    user_thumbnail_set[user_id] = False
+    await message.reply_text("ᴛʜᴜᴍʙɴᴀɪʟ ꜱᴇᴛ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ! ✅", quote=True)
+
+@Client.on_message(filters.private & filters.command(['view_thumb']))
+async def view_thumb(client, message):
+    user_id = message.from_user.id
+    thumb = find(user_id)[0]
+    if thumb:
+        await client.send_photo(user_id, photo=thumb, quote=True)
+    else:
+        await message.reply_text("**ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴛʜᴜᴍʙɴᴀɪʟ ꜱᴇᴛ**", quote=True)
+
+@Client.on_message(filters.private & filters.command(['del_thumb']))
+async def remove_thumb(client, message):
+    user_id = message.from_user.id
+    delthumb(user_id)
+    await message.reply_text("**ᴛʜᴜᴍʙɴᴀɪʟ ᴅᴇʟᴇᴛᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ**", quote=True)
 
