281d30f56d9c008f19654c01de2c0a67e01b86b3
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-19 00:18:35 +0530

    Update myplan.py

--- plugins/myplan.py
+++ plugins/myplan.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime, date
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from helper.database import find_one, used_limit, daily, uploadlimit, usertype
@@ -8,19 +9,17 @@ from helper.progress import humanbytes
 @Client.on_message(filters.private & filters.command(["myplan"]))
 async def myplan(client, message):
     user_data = find_one(message.from_user.id)
-    limit = user_data["uploadlimit"]
-    used = user_data["used_limit"]
-    user_type = user_data["usertype"]
-    pre_expiration = user_data["prexdate"]
+    limit = user_data.get("uploadlimit", 0)
+    used = user_data.get("used_limit", 0)
+    user_type = user_data.get("usertype", "Free")
+    pre_expiration = user_data.get("prexdate")
 
-    if pre_expiration:
-        pre_check = check_expi(pre_expiration)
-        if not pre_check:
-            uploadlimit(message.from_user.id, 16384000000)
-            usertype(message.from_user.id, "Free")
+    if pre_expiration and not check_expi(pre_expiration):
+        uploadlimit(message.from_user.id, 16384000000)
+        usertype(message.from_user.id, "Free")
 
-    if pre_expiration is None or check_expi(pre_expiration) is False:
-        today = int(time.mktime(time.strptime(str(date.today()), '%Y-%m-%d')))
+    if not pre_expiration or not check_expi(pre_expiration):
+        today = int(time.mktime(date.today().timetuple()))
         daily(message.from_user.id, today)
         used_limit(message.from_user.id, 0)
 
@@ -29,17 +28,19 @@ async def myplan(client, message):
     if pre_expiration:
         normal_date = datetime.fromtimestamp(pre_expiration).strftime('%Y-%m-%d')
 
-    text = f"User ID:- `{message.from_user.id}`\nPlan :- {user_type}\nDaily Upload Limit :- {humanbytes(limit)}\nToday Used :- {humanbytes(used)}\nRemain:- {humanbytes(remain)}"
+    text = f"ᴜꜱᴇʀ ɪᴅ:- `{message.from_user.id}`\nᴘʟᴀɴ :- {user_type}\nᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ :- {humanbytes(limit)}\nᴛᴏᴅᴀʏ ᴜꜱᴇᴅ :- {humanbytes(used)}\nʀᴇᴍᴀɪɴ:- {humanbytes(remain)}"
     if normal_date:
-        text += f"\n\nYour Plan Ends On :- {normal_date}"
+        text += f"\n\nʏᴏᴜʀ ᴘʟᴀɴ ᴇɴᴅꜱ ᴏɴ :- {normal_date}"
 
     reply_markup = None
     if user_type == "Free":
         reply_markup = InlineKeyboardMarkup([
             [
-                InlineKeyboardButton("💳 Upgrade", callback_data="upgrade"),
-                InlineKeyboardButton("Cancel ✖️ ", callback_data="cancel")
+                InlineKeyboardButton("💳 ᴜᴘɢʀᴀᴅᴇ", callback_data="upgrade"),
+            ],
+            [
+                InlineKeyboardButton("✖️ ᴄᴀɴᴄᴇʟ ✖️", callback_data="cancel")
             ]
-        ]
-    )
+        ])
     await message.reply(text, quote=True, reply_markup=reply_markup)
+
