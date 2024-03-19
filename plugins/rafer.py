ace492d88e3276d4d7f96e6c1c63b0c7072f3b03
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-19 13:11:19 +0530

    Update rafer.py

--- plugins/rafer.py
+++ plugins/rafer.py
@@ -7,14 +7,14 @@ async def refer(client, message):
     reply_markup = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("⚡ ꜱʜᴀʀᴇ ʏᴏᴜʀ ʟɪɴᴋ", url=f"https://t.me/share/url?url=https://t.me/Zoro_Renamer_bot?start={message.from_user.id}")
+                InlineKeyboardButton("⚡ Share Your Link", url=f"https://t.me/share/url?url=https://t.me/Zoro_Renamer_bot?start={message.from_user.id}")
             ]
         ]
     )
     await client.send_photo(
         chat_id=message.chat.id,
         photo="https://telegra.ph/file/1f1e29702f6cb8fd157e3.jpg",
-        caption="ʀᴇꜰᴇʀ ᴀɴᴅ ᴇᴀʀɴ ɢᴇᴛ 5ɢʙ ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ\nᴘᴇʀ ʀᴇꜰᴇʀ 5ɢʙ",
+        caption="Refer and Earn. Get 5GB Upload Limit per Refer.",
         reply_markup=reply_markup,
         quote=True
     )
